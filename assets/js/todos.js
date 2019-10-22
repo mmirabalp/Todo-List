bb9a85e596a9f96d85304293533db48296a994db
Mawel Mirabal <mmirabalp@gmail.com>
2019-10-21 21:56:12 -0700

    Finish logic and functionality

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,27 +1,23 @@
-// Check off Specific Todos by Clicking
+// Check off Specific Todos by click
+$("ul").on("click", "li", function() {
+    $(this).toggleClass("completed");
+});
 
-$("li").click(function() {
+// On to expand X
+$("ul").on("click", "span", function(event) {
+    $(this).parent().fadeOut(500, function() {
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
 
-    // if li is gray
-    if ($(this).css("color") === "rgb(128, 128, 128)") {
-        // turn it black 
-        $(this).css({
-            color: "black",
-            textDecoration: "none"
-        });
+$("input[type='text']").keypress(function(event) {
+
+    if (event.which === 13) {
+        //    grabbing new todo text from input
+        var todoText = $(this).val();
+        $(this).val("");
+        // create a new li and add to ul
+        $("ul").append("<li><span>X</span> " + todoText + "</li>")
     }
-    // else
-    else {
-        // turn it gray
-        $(this).css({
-            color: "gray",
-            textDecoration: "line-through"
-        });
-    }
-
-
-
-
-
-
 });
